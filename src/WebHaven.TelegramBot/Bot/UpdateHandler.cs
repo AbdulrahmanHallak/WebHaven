129f9b40c20ab0bd247f7ebd2db9ac404b9420fc
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-06-08 20:50:09 +0300

    add simple error handling

--- src/WebHaven.TelegramBot/Bot/UpdateHandler.cs
+++ src/WebHaven.TelegramBot/Bot/UpdateHandler.cs
@@ -1,3 +1,4 @@
+using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using WebHaven.TelegramBot.Bot.Handlers;
@@ -12,29 +13,35 @@ public class UpdateHandler(IServiceScopeFactory scopeFactory)
         switch (update.Type)
         {
             case UpdateType.Message:
-                var msgHandler = scope.ServiceProvider.GetRequiredService<MessageHandler>();
-                await msgHandler.Handle(update.Message!, token);
+                try
+                {
+                    var msgHandler = scope.ServiceProvider.GetRequiredService<MessageHandler>();
+                    await msgHandler.Handle(update.Message!, token);
+                }
+                catch (Exception ex)
+                {
+                    await HandleError(ex, token);
+                }
                 break;
             default:
                 break;
         }
     }
 
-    // TODO: Do something about error handling
-    // public async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-    // {
+    public async Task HandleError(Exception exception, CancellationToken cancellationToken)
+    {
 
-    //     var ErrorMessage = exception switch
-    //     {
-    //         ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-    //         _ => exception.ToString()
-    //     };
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
 
-    //     // _logger.LogInformation("HandleError: {ErrorMessage}", ErrorMessage);
+        // logger.LogInformation("HandleError: {ErrorMessage}", ErrorMessage);
 
-    //     // Cooldown in case of network connection error
-    //     if (exception is RequestException)
-    //         await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
-    // }
+        // Cooldown in case of network connection error
+        if (exception is RequestException)
+            await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
+    }
 
 }
