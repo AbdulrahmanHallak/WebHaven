8d51f6c2da75d285c3ba1bbd505854f6e96de11f
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-06-09 23:45:01 +0300

    Add a console app to execute migrations

---
+++ src/WebHaven.DatabaseMigrator/MigrateUpCommand.cs
@@ -0,0 +1,52 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Oakton;
+
+namespace WebHaven.DatabaseMigrator;
+
+public class MigrateUpCommand : OaktonCommand<MigrateUpInput>
+{
+    private readonly IMigrationRunner _mgRunner;
+    public MigrateUpCommand(IMigrationRunner runner)
+    {
+        _mgRunner = runner;
+        Usage("Migrate the database up")
+        .Arguments(x => x.ConnectionString);
+    }
+    public override bool Execute(MigrateUpInput input)
+    {
+        try
+        {
+            _mgRunner.ListMigrations();
+            _mgRunner.MigrateUp();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        return true;
+    }
+}
+
+
+public class MigrateUpInput
+{
+    [Description("Connection String for the db to which migrations should be applied.")]
+    public string ConnectionString { get; set; } = default!;
+}
+
+public class CommandCreator(IServiceProvider serviceProvider) : ICommandCreator
+{
+    public IOaktonCommand CreateCommand(Type commandType)
+    {
+        using var scope = serviceProvider.CreateScope();
+        return (IOaktonCommand)scope.ServiceProvider.GetRequiredService(commandType);
+    }
+
+    public object CreateModel(Type modelType)
+    {
+        using var scope = serviceProvider.CreateScope();
+        return scope.ServiceProvider.GetRequiredService(modelType);
+    }
+}
---
+++ src/WebHaven.DatabaseMigrator/Program.cs
@@ -0,0 +1,43 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Oakton;
+using Oakton.Help;
+
+namespace WebHaven.DatabaseMigrator;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // The connection string is parsed manually here instead of
+        // using the Oakton library because we need to first initialize
+        // the command in order to parse arguments but we cannot initialize
+        // it without first initializing the migration runner and ServiceProvider
+        // with CreateService method.
+        var serviceProvider = CreateServices(args[1]);
+
+        var executor = CommandExecutor.For(
+            f =>
+            {
+                f.RegisterCommand<MigrateUpCommand>();
+            }
+            , new CommandCreator(serviceProvider));
+
+        executor.Execute(args);
+    }
+
+    static IServiceProvider CreateServices(string connString)
+    {
+        return new ServiceCollection()
+            .AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb
+                .AddPostgres()
+                .WithGlobalConnectionString(connString)
+                .ScanIn(typeof(Program).Assembly).For.Migrations())
+            .AddLogging(lb => lb.AddFluentMigratorConsole())
+            .AddSingleton<MigrateUpCommand>()
+            .AddSingleton<MigrateUpInput>()
+            .AddSingleton<HelpInput>()
+            .BuildServiceProvider(false);
+    }
+}
