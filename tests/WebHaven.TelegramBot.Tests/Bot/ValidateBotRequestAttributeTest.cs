9834198d615deab0a9c3b55407ec36e0c7bec89b
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-06-10 23:17:02 +0300

    adding some tests

---
+++ tests/WebHaven.TelegramBot.Tests/Bot/ValidateBotRequestAttributeTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using NSubstitute;
+using WebHaven.TelegramBot.Bot;
+
+namespace WebHaven.TelegramBot.Tests.Bot;
+
+public class ValidateBotRequestAttributeTest
+{
+
+    [Fact]
+    public async void Request_with_valid_secret_passes()
+    {
+        // Arrange
+        var secret = "Very Important secret";
+        var configs = new BotConfigs() { Secret = secret };
+
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["X-Telegram-Bot-Api-Secret-Token"] = secret;
+
+        var invocationContext = Substitute.For<EndpointFilterInvocationContext>();
+        invocationContext.HttpContext.Returns(httpContext);
+
+        var next = Substitute.For<EndpointFilterDelegate>();
+
+        var filter = new ValidateBotRequestAttribute(configs);
+
+        // Act
+        await filter.InvokeAsync(invocationContext, next);
+
+        // Assert
+        await next.Received(1).Invoke(invocationContext);
+    }
+
+    [Fact]
+    public async Task Request_with_invalid_secret_returns_403()
+    {
+        // Arrange
+        var secret = "Very Important secret";
+        var configs = new BotConfigs { Secret = secret };
+
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["X-Telegram-Bot-Api-Secret-Token"] = "Wrong secret";
+
+        var invocationContextMock = Substitute.For<EndpointFilterInvocationContext>();
+        invocationContextMock.HttpContext.Returns(httpContext);
+
+        var nextMock = Substitute.For<EndpointFilterDelegate>();
+
+        var filter = new ValidateBotRequestAttribute(configs);
+
+        // Act
+        var result = await filter.InvokeAsync(invocationContextMock, nextMock);
+
+        // Assert
+        await nextMock.DidNotReceive().Invoke(invocationContextMock);
+        var problemResult = Assert.IsType<ProblemHttpResult>(result);
+        Assert.Equal(403, problemResult.StatusCode);
+    }
+}
---
+++ tests/WebHaven.TelegramBot.Tests/Feeds/FeedAggregatorTest.cs
@@ -0,0 +1,32 @@
+﻿using WebHaven.TelegramBot.Feeds;
+
+namespace WebHaven.TelegramBot.Tests.Feeds;
+
+public class FeedAggregatorTest
+{
+
+
+    [Fact]
+    public async Task Getting_feed_with_valid_url_succeeds()
+    {
+        var aggregator = new FeedAggregator();
+
+        var feed = await aggregator.GetFeed("https://www.nasa.gov/technology/feed/");
+
+        Assert.NotEmpty(feed);
+        Assert.True(feed.Length > 0);
+    }
+
+    [InlineData("")]
+    [InlineData("https://google.com")]
+    [Theory]
+    public async Task Getting_feed_with_invalid_url_or_not_existent_feed_throws(string url)
+    {
+        var aggregator = new FeedAggregator();
+
+        var act = async () => await aggregator.GetFeed(url);
+
+        var caughtException = await Assert.ThrowsAsync<InvalidOperationException>(act);
+        Assert.Equal("The url you provided is invalid or feed does not exist", caughtException.Message);
+    }
+}
