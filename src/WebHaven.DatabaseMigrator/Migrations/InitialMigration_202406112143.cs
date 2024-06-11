bd6343ecd8b69f744aad51cb479713205b507da1
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-06-11 21:33:47 +0300

    forgot to add those lol

---
+++ src/WebHaven.DatabaseMigrator/Migrations/InitialMigration_202406112143.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+
+namespace WebHaven.DatabaseMigrator;
+
+[Migration(202406112143)]
+public class InitialMigration_202406112143 : Migration
+{
+    public override void Down()
+    {
+        Delete.Table("feeds");
+
+        Delete.Table("users");
+    }
+
+    public override void Up()
+    {
+        var schema = "WebHaven";
+        Create.Schema(schema);
+        Create.Table("users")
+            .InSchema(schema)
+            .WithColumn("id").AsInt64().PrimaryKey()
+            .WithColumn("state").AsString(20).NotNullable();
+
+        Create.Table("feeds")
+            .InSchema(schema)
+            .WithColumn("id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("url").AsString(60).NotNullable()
+            .WithColumn("name").AsString(30).NotNullable()
+            .WithColumn("latest_post_date").AsDateTime().NotNullable()
+            .WithColumn("userId").AsInt64().NotNullable()
+            .ForeignKey("FK_Feeds_Users", schema, "users", "id");
+    }
+}
