0875330fa54c430e39f28fd114b4091a4c3bd3f3
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-07-06 19:46:23 +0300

    modify the tests to reflect filter changes in the last pr

--- tests/WebHaven.TelegramBot.Tests/Bot/ValidateBotRequestAttributeTest.cs
+++ tests/WebHaven.TelegramBot.Tests/Bot/ValidateBotRequestAttributeTest.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
-using NSubstitute;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
 using WebHaven.TelegramBot.Bot;
 
 namespace WebHaven.TelegramBot.Tests.Bot;
@@ -11,50 +13,58 @@ public class ValidateBotRequestAttributeTest
     [Fact]
     public async void Request_with_valid_secret_passes()
     {
-        // Arrange
-        var secret = "Very Important secret";
-        var configs = new BotConfigs() { Secret = secret };
+        var (context, configs) = Arrange("My valid secret");
+        var filter = new ValidateBotRequestFilter(configs);
 
-        var httpContext = new DefaultHttpContext();
-        httpContext.Request.Headers["X-Telegram-Bot-Api-Secret-Token"] = secret;
+        filter.OnActionExecuting(context);
 
-        var invocationContext = Substitute.For<EndpointFilterInvocationContext>();
-        invocationContext.HttpContext.Returns(httpContext);
-
-        var next = Substitute.For<EndpointFilterDelegate>();
-
-        var filter = new ValidateBotRequestAttribute(configs);
-
-        // Act
-        await filter.InvokeAsync(invocationContext, next);
-
-        // Assert
-        await next.Received(1).Invoke(invocationContext);
+        Assert.True(context.Result is not ObjectResult);
     }
 
     [Fact]
-    public async Task Request_with_invalid_secret_returns_403()
+    public void Request_with_invalid_secret_returns_403()
     {
-        // Arrange
-        var secret = "Very Important secret";
+        var (context, configs) = Arrange("My invalid secret", false);
+        var filter = new ValidateBotRequestFilter(configs);
+
+        filter.OnActionExecuting(context);
+
+        Assert.True(context.Result is ObjectResult { StatusCode: 403 });
+    }
+    private MethodInfo GetTestMethodInfoWithValidateAttribute()
+    {
+        return typeof(ValidateBotRequestAttributeTest)
+            .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+            .FirstOrDefault(m => m.GetCustomAttributes(typeof(ValidateBotRequestAttribute), false).Any())!;
+    }
+
+    private (ActionExecutingContext context, BotConfigs configs) Arrange(string secret, bool validSecret = true)
+    {
         var configs = new BotConfigs { Secret = secret };
 
         var httpContext = new DefaultHttpContext();
-        httpContext.Request.Headers["X-Telegram-Bot-Api-Secret-Token"] = "Wrong secret";
+        httpContext.Request.Headers["X-Telegram-Bot-Api-Secret-Token"] = validSecret ? secret : "Wrong secret";
 
-        var invocationContextMock = Substitute.For<EndpointFilterInvocationContext>();
-        invocationContextMock.HttpContext.Returns(httpContext);
+        var actionDescriptor = new ControllerActionDescriptor()
+        {
+            MethodInfo = GetTestMethodInfoWithValidateAttribute()
+        };
+        var controllerContext = new ControllerContext
+        {
+            RouteData = new Microsoft.AspNetCore.Routing.RouteData(),
+            HttpContext = httpContext,
+            ActionDescriptor = actionDescriptor
+        };
 
-        var nextMock = Substitute.For<EndpointFilterDelegate>();
+        var context = new ActionExecutingContext(
+            controllerContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object?>(),
+            null!);
 
-        var filter = new ValidateBotRequestAttribute(configs);
-
-        // Act
-        var result = await filter.InvokeAsync(invocationContextMock, nextMock);
-
-        // Assert
-        await nextMock.DidNotReceive().Invoke(invocationContextMock);
-        var problemResult = Assert.IsType<ProblemHttpResult>(result);
-        Assert.Equal(403, problemResult.StatusCode);
+        return (context, configs);
     }
+
+    [ValidateBotRequest]
+    private void HelperToQueryForAttribute() { }
 }
