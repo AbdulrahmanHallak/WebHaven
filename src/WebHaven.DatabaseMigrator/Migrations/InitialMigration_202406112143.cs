193d4c522826e44adf79d7685c0825d2ef90cf35
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-07-19 21:31:24 +0300

    remove the schema and use public schema
    The whole db is just two tables so there no need
    to have a schema other than the public one.
    I removed the schema because when i introduced it
    i forgot to refactor the sql to use it so removing
    the schema is easier than refactoring all the sql.

--- src/WebHaven.DatabaseMigrator/Migrations/InitialMigration_202406112143.cs
+++ src/WebHaven.DatabaseMigrator/Migrations/InitialMigration_202406112143.cs
@@ -14,20 +14,20 @@ public class InitialMigration_202406112143 : Migration
 
     public override void Up()
     {
-        var schema = "WebHaven";
-        Create.Schema(schema);
+        // TODO: add a migration for introducing indexes and stuff.
+        // TODO: also set the on delete and on update action on the relation
+        // Ids in the db will rely on the Telegram Ids for users so we are not
+        // using an auto-incremented int as the primary key;
         Create.Table("users")
-            .InSchema(schema)
             .WithColumn("id").AsInt64().PrimaryKey()
             .WithColumn("state").AsString(20).NotNullable();
 
         Create.Table("feeds")
-            .InSchema(schema)
             .WithColumn("id").AsInt64().PrimaryKey().Identity()
             .WithColumn("url").AsString(60).NotNullable()
             .WithColumn("name").AsString(30).NotNullable()
             .WithColumn("latest_post_date").AsDateTime().NotNullable()
             .WithColumn("userId").AsInt64().NotNullable()
-            .ForeignKey("FK_Feeds_Users", schema, "users", "id");
+            .ForeignKey("FK_Feeds_Users", "users", "id");
     }
 }
