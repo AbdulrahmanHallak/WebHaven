8b0fb595d60a4f5032dc8ebb92a4d6c6a412a402
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-08-23 18:23:46 +0300

    it is in64 so it is long

--- src/WebHaven.TelegramBot/Feeds/Feed.cs
+++ src/WebHaven.TelegramBot/Feeds/Feed.cs
@@ -1,7 +1,7 @@
 namespace WebHaven.TelegramBot.Feeds;
 public class Feed
 {
-    public required int Id { get; set; }
+    public required long Id { get; set; }
     public required string Name { get; set; }
     public required string Url { get; set; }
     public required DateTime LatestPostDate { get; set; }
