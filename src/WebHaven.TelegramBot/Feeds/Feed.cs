d9dfd08a45f7582b12c0777806471ef68ab9ac45
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-08-25 00:18:51 +0300

    they are not used by anything

--- src/WebHaven.TelegramBot/Feeds/Feed.cs
+++ src/WebHaven.TelegramBot/Feeds/Feed.cs
@@ -1,8 +1,6 @@
 namespace WebHaven.TelegramBot.Feeds;
 public class Feed
 {
-    public required long Id { get; set; }
     public required string Name { get; set; }
     public required string Url { get; set; }
-    public required DateTime LatestPostDate { get; set; }
 };
